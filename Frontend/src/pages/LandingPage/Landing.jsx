6ec6a8d522110c4a9ccf72788f09784a126f4e1b
codebyRIMI <rimihalder1603@gmail.com>
2025-09-23 20:21:13 +0530

    started with landing page

---
+++ Frontend/src/pages/LandingPage/Landing.jsx
@@ -0,0 +1,13 @@
+import './Landing.scss';
+
+
+
+const Landing = () => {
+  return (
+    <div className="landing">
+        <h1>Welcome to the Crop Recommendation System</h1>
+    </div>
+  )
+}
+
+export default Landing
