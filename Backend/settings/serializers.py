964c38fd76827056fdb2650d939522b4c6feb753
Aritra Manna <aritramanna924@gmail.com>
2026-05-29 11:34:06 +0530

    Changes some token

--- Backend/settings/serializers.py
+++ Backend/settings/serializers.py
@@ -1,8 +1,16 @@
 from rest_framework import serializers
 from .models import NotificationPreference
+from .models import UserPreference
 
 class NotificationPreferenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = NotificationPreference
         fields = '__all__'
         read_only_fields = ['user']
+
+#preferences
+class UserPreferenceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserPreference
+        fields = "__all__"
+        read_only_fields = ["user"]
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,9 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { ThemeProvider } from "./context/ThemeContext";
+import "./scss/index.scss";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+   
   </StrictMode>,
 )
