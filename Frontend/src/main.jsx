61294e7d6180fa6ff33a5c92f6c6184d4d9478a0
Aritra Manna <aritramanna924@gmail.com>
2026-05-29 11:37:00 +0530

    Changed main section

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { ThemeProvider } from "./context/ThemeContext";
 import "./scss/index.scss";
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
 
